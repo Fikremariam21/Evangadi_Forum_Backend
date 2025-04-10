ce67d640c9c94a822b36efe1b688ce22cd192972
Fikremariam Mekonnen <fikremariam2012@gmail.com>
2025-04-10 03:30:29 +0300

    Ask question component updated

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -13,7 +13,7 @@ router.post("/create-question", authMiddleware, createQuestion);
 router.put("/update-question/:question_id", authMiddleware, updateQuestion); 
 
 // Use GET for retrieving all questions
-router.get("/api/all/questions", authMiddleware, allQuestions);
+router.get("/all-questions", authMiddleware, allQuestions);
 router.delete("/questions/:question_id", authMiddleware, deleteQuestion); 
 
 
@@ -36,3 +36,4 @@ module.exports = router;
 
 
 
+ 
